69539d8a3c4cc695347f3fb8fb5f199e0cefb0d5
repinamaria <mari.repina.2002@mail.ru>
2021-05-05 21:59:27 +0300

    update

--- src/main/java/directedgraph/DirectedGraph.java
+++ src/main/java/directedgraph/DirectedGraph.java
@@ -21,7 +21,7 @@ public class DirectedGraph {
         @Override
         public boolean equals(Object o){
             if (!(o instanceof Vertex)) return false;
-            return ((Vertex) o).name.equals(this.name);
+            return ((Vertex) o).name.equals(this.name) || o == this;
         }
 
         @Override
@@ -49,7 +49,7 @@ public class DirectedGraph {
         @Override
         public boolean equals(Object o){
             if (!(o instanceof Arc)) return false;
-            return (((Arc) o).from.equals(this.from) && ((Arc) o).to.equals(this.to));
+            return (((Arc) o).from.equals(this.from) && ((Arc) o).to.equals(this.to)) || o == this;
         }
 
         @Override
@@ -60,14 +60,16 @@ public class DirectedGraph {
     Map<String,Vertex> vertexCache = new HashMap<>();
     private Vertex mkVertex(String name){
         Vertex vertex = new Vertex(name);
-        vertexCache.put(name, vertex);
+        if (!hasVertex(vertex.name))
+            vertexCache.put(name, vertex);
         return vertex;
     }
 
     Map<String,Arc> arcCache = new HashMap<>();
     private Arc mkArc(String from, String to, int weight){
-        Arc arc = new Arc(vertexCache.get(from), vertexCache.get(to), weight);
-        arcCache.put(from + "->" + to, arc);
+        Arc arc = new Arc(mkVertex(from), mkVertex(to), weight);
+        if (!hasArc(arc.from.name,arc.to.name))
+            arcCache.put(from + "->" + to, arc);
         return arc;
     }
 
@@ -76,6 +78,10 @@ public class DirectedGraph {
             List<Arc> arcs = new ArrayList<>();
             this.graph.put(mkVertex(name), arcs);
         }
+    }
+
+    public void  addVertex(Vertex vertex) {
+        addVertex(vertex.name);
     }
 
     private boolean hasVertex(String name) {
@@ -90,57 +96,73 @@ public class DirectedGraph {
         if (!hasVertex(from)) addVertex(from);
         if (!hasVertex(to)) addVertex(to);
         if (!hasArc(from, to)) {
-            List<Arc> arcsF = graph.get(vertexCache.get(from));
-            List<Arc> arcsT = graph.get(vertexCache.get(to));
+            List<Arc> arcsF = graph.get(mkVertex(from));
+            List<Arc> arcsT = graph.get(mkVertex(to));
             arcsF.add(mkArc(from, to, weight));
             arcsT.add(mkArc(from, to, weight));
-            this.graph.put(vertexCache.get(from), arcsF);
-            this.graph.put(vertexCache.get(to), arcsT);
+            this.graph.put(mkVertex(from), arcsF);
+            this.graph.put(mkVertex(to), arcsT);
         }
+    }
+
+    public void addArc(Arc arc) {
+        addArc(arc.from.name,arc.to.name,arc.weight);
     }
 
     public void deleteArc(String from, String to) {
         if (hasArc(from, to)) {
-            List<Arc> arcsF = graph.get(vertexCache.get(from));
-            List<Arc> arcsT = graph.get(vertexCache.get(to));
-            arcsF.remove(arcCache.get(from + "->" + to));
-            arcsT.remove(arcCache.get(from + "->" + to));
-            this.graph.put(vertexCache.get(from), arcsF);
-            this.graph.put(vertexCache.get(to), arcsT);
+            List<Arc> arcsF = graph.get(mkVertex(from));
+            List<Arc> arcsT = graph.get(mkVertex(to));
+            arcsF.remove(mkArc(from, to,0));
+            arcsT.remove(mkArc(from, to,0));
+            this.graph.put(mkVertex(from), arcsF);
+            this.graph.put(mkVertex(to), arcsT);
             arcCache.remove(from + "->" + to);
         }
     }
 
+    public void  deleteArc(Arc arc) {
+        deleteArc(arc.from.name, arc.to.name);
+    }
+
     public void deleteVertex(String name) {
         if (hasVertex(name)) {
-            List<Arc> arcs = new ArrayList<>(graph.get(vertexCache.get(name)));
+            List<Arc> arcs = new ArrayList<>(graph.get(mkVertex(name)));
             for (Arc arc : arcs) {
                 deleteArc(arc.from.toString(), arc.to.toString());
             }
-            graph.remove(vertexCache.get(name));
+            graph.remove(mkVertex(name));
             vertexCache.remove(name);
         }
     }
 
-    public void editVertexName(String name, String new_name) {
+    public void  deleteVertex(Vertex vertex) {
+        deleteVertex(vertex.name);
+    }
+
+    public void editVertexName(String name, String newName) {
         if (hasVertex(name)) {
-            List<Arc> arcs = graph.get(vertexCache.get(name));
-            this.graph.put(mkVertex(new_name), arcs);
+            List<Arc> arcs = graph.get(mkVertex(name));
+            this.graph.put(mkVertex(newName), arcs);
             arcs = getOutgoingArcs(name);
             for (Arc arc : arcs) {
-                editArcName(name, arc.to.toString(), new_name, arc.to.toString());
+                editArcName(name, arc.to.toString(), newName, arc.to.toString());
             }
             arcs = getEnteringArcs(name);
             for (Arc arc : arcs) {
-                editArcName(arc.from.toString(), name, arc.from.toString(), new_name);
+                editArcName(arc.from.toString(), name, arc.from.toString(), newName);
             }
             graph.remove(vertexCache.get(name));
         }
     }
 
+    public void editVertexName(Vertex vertex, Vertex newVertex) {
+        editVertexName(vertex.name, newVertex.name);
+    }
+
     private void editArcName(String from, String to, String new_from, String new_to) {
-        for (Arc arc : graph.get(vertexCache.get(from))) {
-            if (arc.from.equals(vertexCache.get(from)) && arc.to.equals(vertexCache.get(to))) {
+        for (Arc arc : graph.get(mkVertex(from))) {
+            if (arc.from.equals(mkVertex(from)) && arc.to.equals(mkVertex(to))) {
                 deleteArc(from, to);
                 addArc(new_from, new_to, arc.weight);
                 break;
@@ -148,33 +170,45 @@ public class DirectedGraph {
         }
     }
 
-    public void editArcWeight(String from, String to, int new_weight) {
+    public void editArcWeight(String from, String to, int newWeight) {
         if (hasArc(from, to)) {
             deleteArc(from, to);
-            addArc(from, to, new_weight);
+            addArc(from, to, newWeight);
         }
+    }
+
+    public void editArcWeight(Arc arc, int newWeight) {
+        editArcWeight(arc.from.name, arc.to.name, newWeight);
     }
 
     public List<Arc> getEnteringArcs(String name) {
         if (hasVertex(name)) {
             List<Arc> arcs = new ArrayList<>();
-            for (Arc arc : graph.get(vertexCache.get(name))) {
-                if (arc.to.equals(vertexCache.get(name)))
+            for (Arc arc : graph.get(mkVertex(name))) {
+                if (arc.to.equals(mkVertex(name)))
                     arcs.add(arc);
             }
             return arcs;
         } else return new ArrayList<>();
     }
 
+    public List<Arc> getEnteringArcs(Vertex vertex) {
+        return getEnteringArcs(vertex.name);
+    }
+
     public List<Arc> getOutgoingArcs(String name) {
         if (hasVertex(name)) {
             List<Arc> arcs = new ArrayList<>();
-            for (Arc arc : graph.get(vertexCache.get(name))) {
-                if (arc.from.equals(vertexCache.get(name)))
+            for (Arc arc : graph.get(mkVertex(name))) {
+                if (arc.from.equals(mkVertex(name)))
                     arcs.add(arc);
             }
             return arcs;
         } else return new ArrayList<>();
+    }
+
+    public List<Arc> getOutgoingArcs(Vertex vertex) {
+        return getOutgoingArcs(vertex.name);
     }
 
     public List<String> getVertexes(){
